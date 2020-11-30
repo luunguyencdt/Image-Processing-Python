f14a67fb26eccca5d85e3769d0f356a3e37d056b
luunguyencdt <luunguyencdt@gmail.com>
2020-11-30 18:31:41 +0900

    Update TrackingObj_using_HSV_colour_space.py

--- TrackingObj_using_HSV_colour_space.py
+++ TrackingObj_using_HSV_colour_space.py
@@ -1,10 +1,17 @@
+
+# Import the neccessary packages
 import cv2
 import numpy as np
 
 def nothing(x):
     pass
 
+# Init camera for capture video
 cap = cv2.VideoCapture(0);
+
+
+# Create tracking bar
+# adjust the thresh-hold value
 
 cv2.namedWindow("Tracking")
 cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
@@ -14,6 +21,8 @@ cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
 cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
 cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
 
+
+# Loops
 while True:
     #frame = cv2.imread('smarties.png')
     _, frame = cap.read()
